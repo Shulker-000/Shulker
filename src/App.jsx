27c9282ccd5739a45c92b533374e9e590a41230c
ADITYA KUMAR GOYAL <adityakumargoyal06@gmail.com>
2025-06-12 19:04:01 +0530

    Whiteboard Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Whiteboard from './components/WhiteBoard'
 
 const App = () => {
   return (
-    <div>
-      COGNIMEET
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/whiteboard" element={<Whiteboard />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/components/WhiteBoard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Tldraw } from '@tldraw/tldraw'
+import '@tldraw/tldraw/tldraw.css'
+import { useNavigate } from 'react-router-dom'
+
+const Whiteboard = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className="relative w-screen h-screen">
+      <Tldraw />
+
+      {/* Floating Go Back Button */}
+      <button
+        onClick={() => navigate('/')}
+        className="absolute bottom-12 right-2 cursor-pointer z-50 px-4 py-2 bg-white bg-opacity-90 text-black text-sm font-semibold border border-gray-300 rounded-lg shadow-md backdrop-blur hover:bg-opacity-100 transition"
+      >
+        Go Back
+      </button>
+    </div>
+  )
+}
+
+export default Whiteboard
